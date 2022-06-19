da1de83b0f99ecf6bbad800e31e1a0c76c10333f
Jim Breslin <jbreslin33@gmail.com>
2022-06-19 11:31:01 -0400

    select all groups

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 token="N5b0MXU4EwlJFEHKv04e0rdZHpFGhK33B6z6nGQ2"
 from groupy.client import Client
 client = Client.from_token(token)
-for group in client.groups.list():
+for group in client.groups.list_all():
     print(group.name)
