1256e6d0ad5c735f131d1ca4a101066007b2331f
Jim Breslin <jbreslin33@gmail.com>
2022-08-08 00:49:19 -0400

    get all messages from all chats

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ from groupy.client import Client
 client = Client.from_token(token)
 for group in client.groups.list_all():
     print(group.name)
+    for message in group.messages.list_all(): 
+        print(message.text)
