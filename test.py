259e1122a1a341935d18f657b27a9e92a974b1b7
Jim Breslin <jbreslin33@gmail.com>
2022-08-10 15:10:02 -0400

    player pool

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ token="N5b0MXU4EwlJFEHKv04e0rdZHpFGhK33B6z6nGQ2"
 from groupy.client import Client
 client = Client.from_token(token)
 for group in client.groups.list_all():
-    print(group.name)
-    g = 'gcal-test'
+    #print(group.name)
+    g = 'Kensington Player Pool'
     if (group.name == g):
         for message in group.messages.list_all(): 
             print(message.text)
